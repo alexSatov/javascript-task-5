a99bd52911ad7431bee2b78822f3fc5699a27c09
Alexander Satov <aleksandr-satov@mail.ru>
2017-10-31 23:25:35 +0500

    first try

--- emitter.js
+++ emitter.js
@@ -1,5 +1,39 @@
 'use strict';
 
+function EventContext(context, handlers) {
+    this.context = context;
+    this.handlers = handlers;
+}
+
+function getSubEvents(event) {
+    const subEvents = [];
+    const events = event.split('.');
+
+    while (events.length !== 0) {
+        subEvents.push(events.join('.'));
+        events.pop();
+    }
+
+    return subEvents;
+}
+
+function isSubEvent(subEvent, event) {
+    return subEvent === event || event.startsWith(subEvent + '.');
+}
+
+function isEventIndexSuitable(eventIndex, eventContext) {
+    const times = eventContext.times;
+    const freq = eventContext.frequency;
+
+    return times === undefined && freq === undefined || (
+        times !== undefined && eventIndex <= times ||
+        freq !== undefined && ((eventIndex - 1) % freq === 0 || eventIndex === 1));
+}
+
+function findEventContext(context, eventContexts) {
+    return eventContexts.filter(evCont => evCont.context === context).pop();
+}
+
 /**
  * Сделано задание на звездочку
  * Реализованы методы several и through
@@ -13,32 +47,75 @@ module.exports = getEmitter;
  */
 function getEmitter() {
     return {
+        events: {},
+        eventsLog: {},
 
         /**
          * Подписаться на событие
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} - emitter
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (event in this.events) {
+                const eventContext = findEventContext(context, this.events[event]);
+
+                if (eventContext !== undefined) {
+                    eventContext.handlers.push(handler);
+                } else {
+                    this.events[event].push(new EventContext(context, [handler]));
+                }
+            } else {
+                this.events[event] = [new EventContext(context, [handler])];
+            }
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} - emitter
          */
         off: function (event, context) {
-            console.info(event, context);
+            const eventsToOff = Object.keys(this.events).filter(e => isSubEvent(event, e));
+
+            for (let eventToOff of eventsToOff) {
+                this.events[eventToOff] = this.events[eventToOff]
+                    .filter(eventContext => eventContext.context !== context);
+            }
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} - emitter
          */
         emit: function (event) {
-            console.info(event);
+            const eventsToEmit = getSubEvents(event);
+
+            while (eventsToEmit.length !== 0) {
+                event = eventsToEmit.shift();
+
+                this.eventsLog[event] = event in this.eventsLog ? this.eventsLog[event] + 1 : 1;
+                const eventIndex = this.eventsLog[event];
+
+                if (event in this.events) {
+                    this.events[event].forEach(
+                        evCont => {
+                            if (isEventIndexSuitable(eventIndex, evCont)) {
+                                evCont.handlers.forEach(h => h.call(evCont.context));
+                            }
+                        }
+                    );
+                }
+            }
+
+            return this;
         },
 
         /**
@@ -48,9 +125,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} - emitter
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            this.on(event, context, handler);
+
+            if (times > 0) {
+                const eventContext = findEventContext(context, this.events[event]);
+                eventContext.times = times;
+            }
+
+            return this;
         },
 
         /**
@@ -60,9 +145,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} - emitter
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            this.on(event, context, handler);
+
+            if (frequency > 0) {
+                const eventContext = findEventContext(context, this.events[event]);
+                eventContext.frequency = frequency;
+            }
+
+            return this;
         }
     };
 }
